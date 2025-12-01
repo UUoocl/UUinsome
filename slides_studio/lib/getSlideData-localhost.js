c84822c38c725c747d3501b37553dd8dfc177a5b
UUoocl <99063397+UUoocl@users.noreply.github.com>
2025-12-01 18:37:37 -0500

    slide table update

--- slides_studio/lib/getSlideData-localhost.js
+++ slides_studio/lib/getSlideData-localhost.js
@@ -1,19 +1,17 @@
 //Messages from iframes in this page
 //Run these functions while the slides iframe is loading
 //stop if the message is not from this site or the slides iframe has loaded
-console.log("message received from currentSlide", event);
+
 window.addEventListener("message", async (event) => {
   if (event.origin !== window.location.origin || slidesLoaded == true) {
     return;
   }
 
   let data = JSON.parse(event.data);
-  //console.log(data)
 
 	//on request from tabulator iframe for slide data
 	//return slideArray variable. 
   if (data.namespace === "tabulator" && data.message === "slides-data") {
-    console.log("got the slide data request");
     studioIframe.contentWindow.postMessage(
       JSON.stringify({
         message: "slides-data-response",
@@ -29,21 +27,7 @@ window.addEventListener("message", async (event) => {
     console.log("Slides ready");
     slidesArray = [];
 
-		//this will trigger the getslide event
-    // currentSlide.contentWindow.postMessage(
-    //   JSON.stringify({ method: "slide", args: [0, 0] }),
-    //   window.location.origin
-    // );
-
     addSlideToSlideArray(presentSlide(), data)
-    
-    //Get slide 1 attributes when deck is ready
-    // const iframe = document.getElementById("current-iframe");
-    // const iframeDocument = iframe.contentDocument;
-    // // Access a specific element by ID within the iframe
-    // const currentEl = iframeDocument.slidesEl.querySelector(".present");    
-    // console.log("iframe current element", currentEl)
-    // addElementToSlideArray(currentEl, data);
 
   }
 
@@ -57,7 +41,6 @@ window.addEventListener("message", async (event) => {
   ) {
     // find current slide attributes
     const slidesProgress = data.result;
-console.log("slidesProgress",slidesProgress)
     if (slidesProgress === 1) {
       tableLoaded = true;
       slidesLoaded = true;
@@ -75,10 +58,6 @@ console.log("slidesProgress",slidesProgress)
         window.location.origin
       );
     } else {
-      if(slidesProgress === 0){
-        //get first slide
-        
-      }
       //get next slide
       currentSlide.contentWindow.postMessage(
         JSON.stringify({ method: "next" }),
@@ -94,28 +73,20 @@ console.log("slidesProgress",slidesProgress)
     tableLoaded === false &&
     data.eventName === "slidechanged"
   ) {
-    console.log("currentEl", data.state, presentSlide())
     addSlideToSlideArray(presentSlide(), data);
-    // currentSlide.contentWindow.postMessage(
-    // JSON.stringify({ method: "getProgress" }),
-    // window.location.origin
-  // );
-  }
-//get next fragment
-  if (
-    data.namespace === "reveal" &&
-    slidesLoaded === false &&
-    tableLoaded === false &&
-    data.eventName === "fragmentshown"
-  ) {
-    console.log("currentEl", data.state, presentSlide())
-    //addSlideToSlideArray(presentSlide(), data);
-    currentSlide.contentWindow.postMessage(
-      JSON.stringify({ method: "getProgress" }),
-      window.location.origin
-    );
-  }
-});
+    }
+    
+    //get next fragment
+    if (
+      data.namespace === "reveal" &&
+      slidesLoaded === false &&
+      tableLoaded === false &&
+      ["fragmentshown","fragmenthidden"].includes(data.eventName)
+    ) {
+      // console.log("currentEl", data.state, presentSlide())
+      addFragmentToSlideArray(presentSlide(), data);
+    }
+  });
 
 let presentSlide = () =>{
   let slideEl = currentSlide.contentDocument.querySelector('.present')
@@ -127,8 +98,6 @@ let presentSlide = () =>{
 }
 
 async function addSlideToSlideArray(slideEl, data) {
-  console.log("function data", data)
-  console.log("slide element", slideEl);
   //Get slide id
   const slideId = await getSlideDataAttributeValue(
     slideEl,
@@ -160,91 +129,21 @@ async function addSlideToSlideArray(slideEl, data) {
     "data-scene"
   );
 
-  const isFragment =
-    data.state.indexf !== undefined ? data.state.indexf : false;
-
   let state = `${data.state.indexh},${data.state.indexv}`;
-  //get all the fragments
-  if (isFragment === -1) {
-    const children = [];
-    const fragments = slideEl.querySelectorAll(".fragment");
-    for (let f_index = 0; f_index < fragments.length; f_index++) {
-      const fragment = fragments[f_index];
 
-      const fragmentId = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-index"
-      );
-
-      const fragmentState = `${data.state.indexh},${data.state.indexv},${fragmentId}`;
-
-      const slideName = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-name"
-      );
-
-      const slidePosition = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-position"
-      );
-
-      const cameraPosition = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-camera-position"
-      );
-
-      const cameraShape = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-camera-shape"
-      );
-      const scene = await getFragmentDataAttributeValue(
-        fragment,
-        "data-fragment-scene"
-      );
-
-      const fragmentDetails = {
-        slideId: `${fragmentId}`,
-        slideState: fragmentState,
-        slideName: slideName,
-        slidePosition: slidePosition,
-        cameraPosition: cameraPosition,
-        cameraShape: cameraShape,
-        scene: scene,
-      };
-      const result = children.find(
-        ({ slideId }) => slideId === `${fragmentId}`
-      );
-      if (typeof result != "object") {
-        children.push(fragmentDetails);
-      }
-    }
-    const slideDetails = {
-      slideId: slideId,
-      slideState: state,
-      slideName: slideName,
-      slidePosition: slidePosition,
-      cameraPosition: cameraPosition,
-      cameraShape: cameraShape,
-      scene: scene,
-      _children: children,
-    };
-    slidesArray.push(slideDetails);
-  }
-
-  if (isFragment === false) {
-    const slideDetails = {
-      slideId: slideId,
-      slideState: state,
-      slideName: slideName,
-      slidePosition: slidePosition,
-      cameraPosition: cameraPosition,
-      cameraShape: cameraShape,
-      scene: scene,
-      // _children: children
-    };
-    slidesArray.push(slideDetails);
-  }
-  console.log("slides array",slidesArray)
+  const slideDetails = {
+    slideId: slideId,
+    slideState: state,
+    slideName: slideName,
+    slidePosition: slidePosition,
+    cameraPosition: cameraPosition,
+    cameraShape: cameraShape,
+    scene: scene,
+    // _children: children
+  };
+  slidesArray.push(slideDetails);
+  
+  // console.log("slides array",slidesArray)
   currentSlide.contentWindow.postMessage(
     JSON.stringify({ method: "getProgress" }),
     window.location.origin
@@ -252,15 +151,66 @@ async function addSlideToSlideArray(slideEl, data) {
   // currentSlide.contentWindow.postMessage(JSON.stringify({ method: 'getSlidesAttributes' }), '*');
 }
 
-async function getSlideDataAttributeValue(element, dataAttribute) {
-  //parser to convert String to HTML Element object
-  // const parser = new DOMParser();
-  // const slideElement = parser.parseFromString(element, "text/html");
-  //if querySelector is nullish, then set the value to an empty string
-  const getEl = element.querySelector(`[${dataAttribute}]`) ?? "";
-  return getEl ? getEl.getAttribute(dataAttribute) : "";
-}
 
-async function getFragmentDataAttributeValue(element, dataAttribute) {
-  return element ? element.getAttribute(dataAttribute) : "";
+async function addFragmentToSlideArray(slideEl, data) {
+// get fragment
+  const slideToFind = `${data.state.indexh},${data.state.indexv}`;
+  const foundSlide = slidesArray.find(slide => slide.slideState === slideToFind);
+  const fragment = {}
+  
+  if (foundSlide) {
+    if (!foundSlide.hasOwnProperty("_children")) {
+      foundSlide._children = []; // Initialize as an empty array if property doesn't exist
+    }
+    const fragmentEl = slideEl.querySelector('.current-fragment')
+    
+    fragment.slideState = `${data.state.indexh},${data.state.indexv},${await getFragmentDataAttributeValue(
+      slideEl,
+      "data-fragment"
+    )}`;
+
+    fragment.slideName = await getFragmentDataAttributeValue(
+        fragmentEl,
+        "data-fragment-name"
+      );
+  
+    fragment.slidePosition = await getFragmentDataAttributeValue(
+      fragmentEl,
+      "data-fragment-position"
+    );
+
+    fragment.cameraPosition = await getFragmentDataAttributeValue(
+      fragmentEl,
+      "data-fragment-camera-position"
+    );
+    
+    fragment.cameraShape = await getFragmentDataAttributeValue(
+      fragmentEl,
+      "data-fragment-camera-shape"
+    );
+
+    fragment.scene = await getFragmentDataAttributeValue(
+      fragmentEl,
+      "data-fragment-scene"
+    );    
+    
+    foundSlide._children.push(fragment);// push the fragment to the children property
+  } else {
+    console.log(`Slide with ID ${slideToFind} not found.`);
+  }
+  currentSlide.contentWindow.postMessage(
+    JSON.stringify({ method: "getProgress" }),
+    window.location.origin
+  );
 }
+                          
+  async function getSlideDataAttributeValue(element, dataAttribute) {
+    //if querySelector is nullish, then set the value to an empty string
+    const getEl = element.querySelector(`[${dataAttribute}]`) ?? "";
+    return getEl ? getEl.getAttribute(dataAttribute) : "";
+  }
+  
+  async function getFragmentDataAttributeValue(element, dataAttribute) {
+    return element ? element.getAttribute(dataAttribute) : "";
+  }
+  
--- slides_studio/lib/getSlideData-slides_com.js
+++ slides_studio/lib/getSlideData-slides_com.js
@@ -1,22 +1,24 @@
-console.log("getSlideData slides.com loaded");
 //Message from Reveal Slides currentSlide API
 window.addEventListener("message", async (event) => {
 	//only run if reveal deck has not been processed
 	//only get the slide data once.  
-	//TOdo remove listener after slide data is processed,
-	//test fragments  
 	if (event.origin !== "https://slides.com" || slidesLoaded == true) {
 		return;
 	}
-	
-	console.log("message received from currentSlide", event);
+
 	let data = JSON.parse(event.data);
 	
 	//on request from tabulator iframe for slide data
 	//return slideArray variable. 
     if(data.namespace === "tabulator" && data.message === "slides-data"){
-		console.log("got the slide data request")
-        studioIframe.contentWindow.postMessage(JSON.stringify({ message:'slides-data-response', slidesData: slidesArray, namespace: 'speakerview'}), window.location.origin);
+        studioIframe.contentWindow.postMessage(
+			JSON.stringify({ 
+				message:'slides-data-response', 
+				slidesData: slidesArray, 
+				namespace: 'speakerview'
+			}), 
+			window.location.origin
+		);
     }
 	
 	//on reveal slides 'ready' start the get Slides and Fragments process 
@@ -31,15 +33,16 @@ window.addEventListener("message", async (event) => {
 		);	
 	}
 
+    //when slide deck progress equals 1, end the get slide process
 	if (
 		data.namespace === "reveal" &&
 		data.eventName === "callback" &&
 		data.method === "getProgress" &&
 		tableLoaded == false  && 
-		slidesLoaded === false) {
-	
+		slidesLoaded === false
+	) {
+	  // find current slide attributes
 		const slidesProgress = data.result;
-	
 		if (slidesProgress === 1) {
 			tableLoaded = true;
 			slidesLoaded = true;
@@ -48,7 +51,14 @@ window.addEventListener("message", async (event) => {
 				"https://slides.com"
 			);
 			//send to data table iframe
-			studioIframe.contentWindow.postMessage(JSON.stringify({ message:'slides-data-response', slidesData: slidesArray, namespace: 'speakerview'}), window.location.origin);
+			studioIframe.contentWindow.postMessage(
+				JSON.stringify({ 
+					message:'slides-data-response', 
+					slidesData: slidesArray, 
+					namespace: 'speakerview'
+				}), 
+				window.location.origin
+			);
 		} else {
 			//get next slide
 			currentSlide.contentWindow.postMessage(
@@ -66,18 +76,21 @@ window.addEventListener("message", async (event) => {
 		tableLoaded === false && 
         slidesLoaded === false
 	) {
-		console.log(event);
 		//slides HTML is returned in string format
 		const slide = data.result;
 		//parser to convert String to HTML Element object
 		const parser = new DOMParser();
-
+		
 		const slideEl = parser.parseFromString(
 			slide.originalSlideHTML,
 			"text/html"
 		);
+		
 		//Get slide id
-		const slideId = await getSlideElementAttributeValue(slideEl, "data-id");
+		const slideId = await getSlideElementAttributeValue(
+			slideEl, 
+			"data-id"
+		);
 
 		const slideName = await getSlideElementAttributeValue(
 			slideEl,
@@ -104,97 +117,100 @@ window.addEventListener("message", async (event) => {
 			"data-scene"
 		);
 
-		const isFragment =
-			data.state.indexf !== undefined ? data.state.indexf : false;
-
 		let state = `${data.state.indexh},${data.state.indexv}`;
+		// const isFragment =
+		// 	data.state.indexf !== undefined ? data.state.indexf : false;
 
-		//get all the fragments
-		if (isFragment === -1) {
-			const children = [];
-			const fragments = slideEl.querySelectorAll(".fragment");
-			console.log("fragment count", fragments.length);
-			for (let f_index = 0; f_index < fragments.length; f_index++) {
-				const fragment = fragments[f_index];
 
-				const fragmentId = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-index"
-				);
+		// //get all the fragments
+		// if (data.state.indexf !== undefined) {
+		// // console.log("data results",data)
+		// // console.log("getslide result",slideEl);
+		
+		// 	const children = [];
+		// 	const fragments = slideEl.querySelectorAll(".fragment");
+		// 	console.log("fragment count", fragments.length);
+		// 	for (let f_index = 0; f_index < fragments.length; f_index++) {
+		// 		const fragment = fragments[f_index];
 
-				const fragmentState = `${data.state.indexh},${data.state.indexv},${fragmentId}`;
+		// 		const fragmentId = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-index"
+		// 		);
 
-				const slideName = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-name"
-				);
+		// 		const fragmentState = `${data.state.indexh},${data.state.indexv},${fragmentId}`;
 
-				const slidePosition = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-position"
-				);
+		// 		const slideName = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-name"
+		// 		);
 
-				const cameraPosition = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-camera-position"
-				);
+		// 		const slidePosition = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-position"
+		// 		);
 
-				const cameraShape = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-camera-shape"
-				);
+		// 		const cameraPosition = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-camera-position"
+		// 		);
 
-				const scene = await getFragmentDataAttributeValue(
-					fragment,
-					"data-fragment-scene"
-				);
+		// 		const cameraShape = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-camera-shape"
+		// 		);
 
-				const fragmentDetails = {
-					slideId: `${fragmentId}`,
-					slideState: fragmentState,
-					slideName: slideName,
-					slidePosition: slidePosition,
-					cameraPosition: cameraPosition,
-					cameraShape: cameraShape,
-					scene: scene,
+		// 		const scene = await getFragmentDataAttributeValue(
+		// 			fragment,
+		// 			"data-fragment-scene"
+		// 		);
+
+		// 		const fragmentDetails = {
+		// 			slideId: `${fragmentId}`,
+		// 			slideState: fragmentState,
+		// 			slideName: slideName,
+		// 			slidePosition: slidePosition,
+		// 			cameraPosition: cameraPosition,
+		// 			cameraShape: cameraShape,
+		// 			scene: scene,
                     
-				};
-				const result = children.find(
-					({ slideId }) => slideId === `${fragmentId}`
-				);
-				console.log(result);
-				if (typeof result != "object") {
-					children.push(fragmentDetails);
-				}
-			}
-			const slideDetails = {
-				slideId: slideId,
-				slideState: state,
-				slideName: slideName,
-				slidePosition: slidePosition,
-				cameraPosition: cameraPosition,
-				cameraShape: cameraShape,
-				scene: scene,
+		// 		};
+		// 		const result = children.find(
+		// 			({ slideId }) => slideId === `${fragmentId}`
+		// 		);
+		// 		// console.log(result);
+		// 		if (typeof result != "object") {
+		// 			children.push(fragmentDetails);
+		// 		}
+		// 	}
+		// 	const slideDetails = {
+		// 		slideId: slideId,
+		// 		slideState: state,
+		// 		slideName: slideName,
+		// 		slidePosition: slidePosition,
+		// 		cameraPosition: cameraPosition,
+		// 		cameraShape: cameraShape,
+		// 		scene: scene,
                 
-				_children: children,
-			};
-			slidesArray.push(slideDetails);
-		}
+		// 		// _children: children,
+		// 	};
+		// 	slidesArray.push(slideDetails);
+		// }
 
-		if (isFragment === false) {
-			const slideDetails = {
-				slideId: slideId,
-				slideState: state,
-				slideName: slideName,
-				slidePosition: slidePosition,
-				cameraPosition: cameraPosition,
-				cameraShape: cameraShape,
-				scene: scene,
-                
-				// _children: children
-			};
-			slidesArray.push(slideDetails);
-		}
+		const slideDetails = {
+			slideId: slideId,
+			slideState: state,
+			slideName: slideName,
+			slidePosition: slidePosition,
+			cameraPosition: cameraPosition,
+			cameraShape: cameraShape,
+			scene: scene,
+			
+			// _children: children
+		};
+
+		slidesArray.push(slideDetails);
+	
 
 		currentSlide.contentWindow.postMessage(
 			JSON.stringify({ method: "getProgress" }),
@@ -203,16 +219,17 @@ window.addEventListener("message", async (event) => {
 	}
 
 
+	// new logic 
+	// if slidechanged then getSlide
+	// if fragmentshown or fragmenthidden then append to slidearray 
 	if (
-		data.namespace === "reveal" && slidesLoaded === false && tableLoaded === false &&
-		["fragmentshown", "fragmenthidden", "slidechanged"].includes(
-			data.eventName
-		)
+		data.namespace === "reveal" &&
+		 slidesLoaded === false && 
+		 tableLoaded === false &&
+		data.eventName === "slidechanged"
 	) {
 		slideState = data.state;
-		slideState.indexf = data.state.indexf ?? "";
-		console.log(slideState);
-
+		
 		//Get slides attributes
 		currentSlide.contentWindow.postMessage(
 			JSON.stringify({
@@ -222,23 +239,47 @@ window.addEventListener("message", async (event) => {
 			"https://slides.com"
 		);
 	}
+	
+	if (
+		data.namespace === "reveal" &&
+		slidesLoaded === false && 
+		tableLoaded === false &&
+		["fragmentshown", "fragmenthidden"].includes(
+			data.eventName
+		)	
+	){
+		addFragmentToSlideArray(data);
+	}
 });
 
 async function getSlideElementAttributeValue(element, dataAttribute) {
 	//if querySelector is nullish, then set the value to an empty string
 	const getEl = element.querySelector(`[${dataAttribute}]`) ?? "";
-
 	return getEl ? getEl.getAttribute(dataAttribute) : "";
-}
-
-function getSlide() {
-	console.log("calling get slide");
-	currentSlide.contentWindow.postMessage(
-		JSON.stringify({ method: "getSlide" }),
-		"https://slides.com"
-	);
 }
 
 async function getFragmentDataAttributeValue(element, dataAttribute){
    return element ? element.getAttribute(dataAttribute): "";
+}
+
+async function addFragmentToSlideArray(data) {
+// get fragment
+  const slideToFind = `${data.state.indexh},${data.state.indexv}`;
+  const foundSlide = slidesArray.find(slide => slide.slideState === slideToFind);
+  const fragment = {}
+
+  if (foundSlide) {
+    if (!foundSlide.hasOwnProperty("_children")) {
+      foundSlide._children = []; // Initialize as an empty array if property doesn't exist
+    }
+    fragment.slideState = `${data.state.indexh},${data.state.indexv},${data.state.indexf}`;
+    
+    foundSlide._children.push(fragment);// push the fragment to the children property
+  } else {
+    console.log(`Slide with ID ${slideToFind} not found.`);
+  }
+  currentSlide.contentWindow.postMessage(
+    JSON.stringify({ method: "getProgress" }),
+    "https://slides.com"
+  );
 }
